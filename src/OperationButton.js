88866bd7c6c55027eca1508d2e690d38c0e79e7d
Vidya V <102847191+122004vv@users.noreply.github.com>
2023-11-27 22:58:39 +0530

    Add files via upload

---
+++ src/OperationButton.js
@@ -0,0 +1,13 @@
+import { ACTIONS } from "./App"
+//Vidya V - Practice -27/11/2023
+export default function OperationButton({ dispatch, operation }) {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  )
+}
